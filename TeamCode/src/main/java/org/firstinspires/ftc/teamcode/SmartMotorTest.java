c6bfce9bc060289f9bfc75ba53c944f8d00bb71e
Sean Lee <seanlee@Seans-MacBook-Pro-2.local>
2020-10-06 16:44:42 -0400

    Comment SmartMotor and SmartMotorTest class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SmartMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SmartMotorTest.java
@@ -7,35 +7,35 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous
+// class to test SmartMotors
 public class SmartMotorTest extends AdvancedOpMode {
+    // SmartMotor fields
     SmartMotor frontRightDrive;
     SmartMotor frontLeftDrive;
     SmartMotor backLeftDrive;
     SmartMotor backRightDrive;
+    // boolean used to loop only once
     boolean once = true;
-    int targetVal;
 
     @Override
     public void init() {
-        frontLeftDrive = new SmartMotor(hardwareMap.get(DcMotor.class, "FrontLeftDrive"), WheelPosition.FRONT_LEFT, 16, 5);
-        frontRightDrive = new SmartMotor(hardwareMap.get(DcMotor.class, "FrontRightDrive"), WheelPosition.FRONT_RIGHT, 16, 5);
-        backLeftDrive = new SmartMotor(hardwareMap.get(DcMotor.class, "BackLeftDrive"), WheelPosition.BACK_LEFT, 16, 5);
-        backRightDrive = new SmartMotor(hardwareMap.get(DcMotor.class, "BackRightDrive"), WheelPosition.BACK_RIGHT, 16, 5);
-        targetVal = backLeftDrive.getTarget(20);
+        // initialize all the SmartMotors (assumes that the reduction is 16 and wheelRadius is 5)
+        frontLeftDrive = new SmartMotor(hardwareMap.get(DcMotor.class, "FrontLeftDrive"), WheelPosition.FRONT_LEFT, 16);
+        frontRightDrive = new SmartMotor(hardwareMap.get(DcMotor.class, "FrontRightDrive"), WheelPosition.FRONT_RIGHT, 16);
+        backLeftDrive = new SmartMotor(hardwareMap.get(DcMotor.class, "BackLeftDrive"), WheelPosition.BACK_LEFT, 16);
+        backRightDrive = new SmartMotor(hardwareMap.get(DcMotor.class, "BackRightDrive"), WheelPosition.BACK_RIGHT, 16);
     }
 
     @Override
     public void loop() {
+        // run only once
         if (once) {
+            // robot moves forwards by 20cm (right motors are flipped)
             frontLeftDrive.drive(20, 0.5);
             frontRightDrive.drive(-20, 0.5);
             backLeftDrive.drive(20, 0.5);
             backRightDrive.drive(-20, 0.5);
             once = false;
         }
-        double encoderVal = backLeftDrive.getCount();
-        telemetry.addData("backLeftDrive Encoder Value:", encoderVal);
-        telemetry.addData("target: ", targetVal);
-        telemetry.update();
     }
 }
