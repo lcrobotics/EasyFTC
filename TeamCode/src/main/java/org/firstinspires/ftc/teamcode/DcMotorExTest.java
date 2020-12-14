849607aa609db765f852e56444965da4e21a3622
ashermkn1 <ashkornfeld@gmail.com>
2020-12-14 16:37:28 -0500

    add DcMotorEx test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DcMotorExTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DcMotorExTest.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.PIDCoefficients;
 
+@Autonomous
 public class DcMotorExTest extends OpMode {
 
     public DcMotorEx FrontLeftDrive = null;
@@ -24,9 +26,23 @@ public class DcMotorExTest extends OpMode {
         FrontRightDrive.setDirection(DcMotor.Direction.REVERSE);
         BackLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         BackRightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        FrontLeftDrive.setPositionPIDFCoefficients(5);
+        FrontRightDrive.setPositionPIDFCoefficients(5);
+        BackLeftDrive.setPositionPIDFCoefficients(5);
+        BackRightDrive.setPositionPIDFCoefficients(5);
+
+        FrontLeftDrive.setVelocityPIDFCoefficients(2, 0.5, 11, 0);
+        FrontRightDrive.setVelocityPIDFCoefficients(2, 0.5, 11, 0);
+        BackLeftDrive.setVelocityPIDFCoefficients(2, 0.5, 11, 0);
+        BackRightDrive.setVelocityPIDFCoefficients(2, 0.5, 11, 0);
     }
 
     @Override
     public void loop() {
+        FrontLeftDrive.setVelocity(14.26);
+        FrontRightDrive.setVelocity(14.26);
+        BackLeftDrive.setVelocity(14.26);
+        BackRightDrive.setVelocity(14.26);
     }
 }
