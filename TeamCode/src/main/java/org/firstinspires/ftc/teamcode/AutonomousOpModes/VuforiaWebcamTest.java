2b57c79b610bdd8e1225b902864b4284d71db25b
ashermkn1 <ashkornfeld@gmail.com>
2020-12-17 16:27:36 -0500

    fix gradle and untract build files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/VuforiaWebcamTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/VuforiaWebcamTest.java
@@ -29,7 +29,6 @@
 
 package org.firstinspires.ftc.teamcode.AutonomousOpModes;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.vuforia.Image;
