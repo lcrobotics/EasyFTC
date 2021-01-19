fc4fc2204493b4bdcfa3885d67b79fd76a121b30
Mathtician <mathtician@gmail.com>
2021-01-19 17:04:42 -0500

    create VuforiaSuperOp class which includes instances of VuforiaFrameGetter and ObjectLocator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/VuforiaSuperOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/VuforiaSuperOp.java
@@ -2,26 +2,30 @@ package org.firstinspires.ftc.teamcode.AutonomousOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.vuforia.Image;
 import com.vuforia.PIXEL_FORMAT;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.teamcode.ObjectLocator;
 import org.firstinspires.ftc.teamcode.VuforiaFrameGetter;
 
-import java.util.Locale;
-
 @Autonomous
-public class DetectColor extends OpMode {
+public abstract class VuforiaSuperOp extends OpMode {
+
     private final static String VUFORIA_KEY = "ARgYuCf/////AAABmUYfc1+dVEQsgUBCPA2kCAFRmuTRB/XUfAJzLsRyFDRg6uMMjj6EXM8YNiY5l3oTw83H+PKgfF46gctdzrln2nnVXMebpgN9ULy1cOfdSsPk0hwSZqzcY0LWCj+rPPrZ3JyQT7gf2aw7bo8ZvWedWB7skuGIjg+9cyTJdDyXmXrQ8Bo4r4siTFNTVFxg21OH/Gd8wrVJF4RqjE+kcez3MzcnE2EPCqWTNixSge5yLg+tN87/R/dMPzqHWvmjE6F6J/7/sahPt7FQ9G6tYWnV1impzZsH7T/JT6pGr2SALwHdaNjBGbYY76ZfvAxixEdob9g6qMBhKOyLg6HTP9VzRZ06ksUhErmR2K2LSkyjxBBz";
+    private static final float mmPerInch = 25.4f;
 
     private VuforiaLocalizer vuforia;
 
+    // Class Members
+    private OpenGLMatrix lastLocation = null;
     private VuforiaFrameGetter frameGetter = null;
+    private ObjectLocator objectLocator = null;
 
-    @Override
-    public void init() {
+    public void init(){
         /*
          * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
          */
@@ -30,11 +34,10 @@ public class DetectColor extends OpMode {
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        parameters.useExtendedTracking = false;
 
         //  Instantiate the Vuforia engine
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
 
         // This is necessary for getting pixels (integral image goal detection, etc)
         boolean[] results = vuforia.enableConvertFrameToFormat(PIXEL_FORMAT.RGB565, PIXEL_FORMAT.YUV);
@@ -43,30 +46,11 @@ public class DetectColor extends OpMode {
         }
         vuforia.setFrameQueueCapacity(1);
         frameGetter = new VuforiaFrameGetter(vuforia.getFrameQueue());
-    }
 
-    @Override
-    public void loop() {
-        frameGetter.updateFrame();
-        telemetry.addData("image dims", String.format(Locale.US, "%d %d", frameGetter.imgWidth, frameGetter.imgHeight));
-        /*
-        for (int h = 0; h < 5; h++) {
-            for (int j = 0; j < 5; j++) {
-                int red = frameGetter.rgbValues[0][h][j];
-                int green = frameGetter.rgbValues[1][h][j];
-                int blue = frameGetter.rgbValues[2][h][j];
-                telemetry.addData(String.format(Locale.US, "pixel %d %d", h, j),
-                        String.format(Locale.US, "red: %d green: %d blue: %d", red, green, blue));
-            }
-        }
-         */
-        int w = 100, h = 100;
-        frameGetter.updateMaxRect(0, w, h);
-        telemetry.addData(
-                String.format(Locale.US, "Max %dx%d rect", w, h),
-                String.format(Locale.US, "%d %d", frameGetter.xMax, frameGetter.yMax));
-        //telemetry.addData("Rect sum", frameGetter.sumOfRect(0, 0, 0, 200, 200));
-        telemetry.update();
+        // Load the data sets for the trackable objects. These particular data
+        // sets are stored in the 'assets' part of our application.
+        VuforiaTrackables targetsUltimateGoal = this.vuforia.loadTrackablesFromAsset("UltimateGoal");
+        objectLocator = new ObjectLocator(targetsUltimateGoal);
+        targetsUltimateGoal.activate();
     }
 }
-
