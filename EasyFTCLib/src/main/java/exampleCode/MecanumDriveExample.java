7e56fda4cba6d08bbd204d6023389e91f9a41b5d
Alexa <alexa_becker@loomis.org>
2021-02-09 17:10:08 -0500

    improve example code

--- EasyFTCLib/src/main/java/exampleCode/MecanumDriveExample.java
+++ EasyFTCLib/src/main/java/exampleCode/MecanumDriveExample.java
@@ -5,28 +5,31 @@ import com.lcrobotics.easyftclib.commandCenter.hardware.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class MecanumDriveExample extends OpMode {
+    // Motor Counts per Revolution
+    final int cpr = 448;
+    // Motor Revolutions per Minute
+    final int rpm = 64;
     // declare Motors
     Motor FrontLeftDrive, FrontRightDrive, BackLeftDrive, BackRightDrive;
     // declare drive constructor
     MecanumDrive drive;
+
     @Override
     public void init() {
-        // Motor Counts per Revolution
-        double cpr = 12;
-        // Motor Revolutions per Minute
-        double rpm = 7;
         // initialize motors
         FrontLeftDrive = new Motor(hardwareMap, "FrontLeftDrive", cpr, rpm);
         FrontRightDrive = new Motor(hardwareMap, "FrontRightDrive", cpr, rpm);
         BackLeftDrive = new Motor(hardwareMap, "BackLeftDrive", cpr, rpm);
         BackRightDrive = new Motor(hardwareMap, "BackRightDrive", cpr, rpm);
+
         // initialize drive
-        drive  = new MecanumDrive(true, 5, FrontLeftDrive, FrontRightDrive, BackLeftDrive, BackRightDrive);
+        drive = new MecanumDrive(true, FrontLeftDrive, FrontRightDrive, BackLeftDrive, BackRightDrive);
     }
 
     @Override
     public void loop() {
         // move forward (values correspond to desired motor powers)
-        drive.drive(0, 1, 0);
+        // square squares all the inputs so drivers have greater control
+        drive.drive(-gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x, true);
     }
 }
