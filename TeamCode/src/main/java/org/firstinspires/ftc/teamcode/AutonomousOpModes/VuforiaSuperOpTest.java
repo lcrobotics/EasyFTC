89b848089dc3c471903b92c969030d8e2c27e16c
Mathtician <mathtician@gmail.com>
2021-02-08 21:59:56 -0500

    move VuforiaSuperOpTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/VuforiaSuperOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/VuforiaSuperOpTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.AutonomousOpModes.FTCExamples;
+package org.firstinspires.ftc.teamcode.AutonomousOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
